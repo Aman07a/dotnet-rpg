1eb92587b481dcf2f1b7b50395b848e8498f67b6
Aman Ahmed <aman16.aa17@gmail.com>
2023-02-15 10:22:09 +0100

    New Controller & GET a New Character

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,22 @@
+using System;
+using dotnet_rpg.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_rpg.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CharacterController : ControllerBase
+    {
+        private static Character knight = new Character();
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(knight);
+        }
+    }
+}
--- Models/Character.cs
+++ Models/Character.cs
@@ -1,3 +1,9 @@
+using System;
+using dotnet_rpg.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
 namespace dotnet_rpg.Models
 {
     public class Character
