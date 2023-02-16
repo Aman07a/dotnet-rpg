1574ff873a23942c80f701910a75081a4cfcfe00
Aman Ahmed <aman16.aa17@gmail.com>
2023-02-16 09:42:47 +0100

    PUT Implementations

--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -59,7 +59,8 @@ namespace dotnet_rpg.Services.CharacterService
 
             try
             {
-                var character = characters.FirstOrDefault(c => c.Id == updatedCharacter.Id);
+                var character = 
+                    await _context.Characters.FirstOrDefaultAsync(c => c.Id == updatedCharacter.Id);
 
                 if (character is null)
                     throw new Exception($"Character with Id '{updatedCharacter.Id}' not found.");
@@ -71,6 +72,7 @@ namespace dotnet_rpg.Services.CharacterService
                 character.Intelligence = updatedCharacter.Intelligence;
                 character.Class = updatedCharacter.Class;
 
+                await _context.SaveChangesAsync();
                 serviceReponse.Data = _mapper.Map<GetCharacterDTO>(character);
             }
             catch (Exception ex)
